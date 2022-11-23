aa6a5187c0251520d26e220ad98e6a8bed834a1b
Orlando Maussa <orlando.maussa@gmail.com>
2022-11-22 19:40:09 -0500

    adding git blame on idle

---
+++ lua/blame.lua
@@ -0,0 +1,59 @@
+local M = {}
+local api = vim.api
+
+if vim.fn.executable('git') == 0 then
+    return
+end
+
+local bufnum = 0
+local namespace = 2
+
+function M.blameVirtText()
+    local ft = vim.fn.expand('%:h:t') -- get the current file extension
+    if ft == '' then -- if we are in a scratch buffer or unknown filetype
+        return
+    end
+    if ft == 'bin' then -- if we are in nvim's terminal window
+        return
+    end
+    api.nvim_buf_clear_namespace(0, namespace, 0, -1) -- clear out virtual text from namespace 2 (the namespace we will set later)
+    local currFile = vim.fn.expand('%')
+    local line = api.nvim_win_get_cursor(0)
+    local blame = vim.fn.system(string.format('git blame -c -L %d,%d %s', line[1], line[1], currFile))
+    local hash = vim.split(blame, '%s')[1]
+    local cmd = string.format("git show %s ", hash) .. "--format='%an | %ar | [ %s ]'"
+    if hash == '00000000' then
+        text = 'Not Committed Yet'
+    else
+        text = vim.fn.system(cmd)
+        text = vim.split(text, '\n')[1]
+        if text:find("fatal") then -- if the call to git show fails
+            text = 'Not Committed Yet'
+        end
+    end
+    api.nvim_buf_set_extmark(
+        bufnum,
+        namespace,
+        line[1] - 1,
+        0,
+        {
+            hl_mode = "combine",
+            virt_text = {
+                { "      " .. text, "GitLens" },
+            }
+        }
+    ) -- set virtual text for namespace 2 with the content from git and assign it to the higlight group 'GitLens'
+end
+
+function M.clearBlameVirtText() -- important for clearing out the text when our cursor moves
+    api.nvim_buf_clear_namespace(0, namespace, 0, -1)
+end
+
+api.nvim_command [[autocmd CursorHold   * lua require'utils'.blameVirtText()]]
+api.nvim_command [[autocmd CursorMoved  * lua require'utils'.clearBlameVirtText()]]
+api.nvim_command [[autocmd CursorMovedI * lua require'utils'.clearBlameVirtText()]]
+api.nvim_set_hl(0, "GitLens", {
+    link = "Comment"
+})
+
+return M
--- plugin/null-ls.rc.lua
+++ plugin/null-ls.rc.lua
@@ -4,35 +4,38 @@ if (not status) then return end
 local augroup_format = vim.api.nvim_create_augroup('Format', { clear = true })
 
 local on_attach = function(client, bufnr)
-  if client.server_capabilities.documentFormattingProvider then
-    vim.api.nvim_clear_autocmds { buffer = 0, group = augroup_format }
-    vim.api.nvim_create_autocmd('BufWritePre', {
-      group = augroup_format,
-      buffer = 0,
-      callback = function () vim.lsp.buf.formatting_seq_sync() end
-    })
-    vim.cmd("nnoremap <silent><buffer> <Leader>f :lua vim.lsp.buf.formatting()<CR>")
-  end
-  if client.server_capabilities.documentRangeFormattingProvider then
-    vim.cmd("xnoremap <silent><buffer> <Leader>f :lua vim.lsp.buf.range_formatting({})<CR>")
-  end
+    if client.server_capabilities.documentFormattingProvider then
+        vim.api.nvim_clear_autocmds { buffer = 0, group = augroup_format }
+        vim.api.nvim_create_autocmd('BufWritePre', {
+            group = augroup_format,
+            buffer = 0,
+            callback = function() vim.lsp.buf.format() end
+        })
+        vim.cmd("nnoremap <silent><buffer> <Leader>f :lua vim.lsp.buf.format()<CR>")
+    end
+    if client.server_capabilities.documentRangeFormattingProvider then
+        vim.cmd("xnoremap <silent><buffer> <Leader>f :lua vim.lsp.buf.range_formatting({})<CR>")
+    end
 end
 
 local sources = {
-  null_ls.builtins.diagnostics.eslint_d.with({
-    diagnostics_format = '[eslint] #{m}\n(#{c})',
-    condition = function(utils)
-      --return utils.root_has_file({'.eslintrc.js', '.eslintrc.cjs', '.eslintrc.yaml', '.eslintrc.yml', '.eslintrc.json'})
-      return utils.root_has_file({'node_modules/.bin/eslint'})
-    end
-  }),
-  null_ls.builtins.diagnostics.zsh,
-  null_ls.builtins.formatting.goimports,
-  null_ls.builtins.formatting.gofmt,
-  null_ls.builtins.formatting.goimports_reviser,
+    null_ls.builtins.diagnostics.eslint_d.with({
+        diagnostics_format = '[eslint] #{m}\n(#{c})',
+        condition = function(utils)
+            --return utils.root_has_file({'.eslintrc.js', '.eslintrc.cjs', '.eslintrc.yaml', '.eslintrc.yml', '.eslintrc.json'})
+            return utils.root_has_file({ 'node_modules/.bin/eslint' })
+        end
+    }),
+    null_ls.builtins.diagnostics.zsh,
+    null_ls.builtins.formatting.stylua,
+    null_ls.builtins.formatting.goimports,
+    null_ls.builtins.formatting.gofmt,
+    null_ls.builtins.formatting.goimports_reviser,
 }
 
 null_ls.setup({
-  sources = sources,
-  on_attach = on_attach
+    sources = sources,
+    on_attach = on_attach
 })
+
+vim.cmd('map <Leader>ln :NullLsInfo<CR>')
